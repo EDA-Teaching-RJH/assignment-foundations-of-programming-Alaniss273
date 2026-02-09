c6eb4ec9c1cc88a8ec9dc2782826fa1aad881b06
Alaniss273 <ah2195@kent.ac.uk>
2026-02-09 14:02:04 +0000

    qw

---
+++ fleet_manager.py
@@ -0,0 +1,5 @@
+n = ["Kirk", "Spock", "Picard", "Janeway", "Sisko"]
+r = ["Captain", "Commander", "Captain", "Captain", "Captain"]
+d = ["Command", "SCience", "Command", "Command", "Command"]
+id = ["SC 937-0176 CEC", "S 179-276 SP", "SP 937-215", "SJ 854-336", "SS 682-190"]
+
