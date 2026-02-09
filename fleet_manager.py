079da7367e06f97b8789a4e15bd9b076e7e2a9bf
Alaniss273 <ah2195@kent.ac.uk>
2026-02-09 14:05:28 +0000

    qe

--- fleet_manager.py
+++ fleet_manager.py
@@ -1,5 +1,10 @@
-n = ["Kirk", "Spock", "Picard", "Janeway", "Sisko"]
-r = ["Captain", "Commander", "Captain", "Captain", "Captain"]
-d = ["Command", "SCience", "Command", "Command", "Command"]
-id = ["SC 937-0176 CEC", "S 179-276 SP", "SP 937-215", "SJ 854-336", "SS 682-190"]
+def init_database():
+    n = ["Kirk", "Spock", "Picard", "Janeway", "Sisko"]
+    r = ["Captain", "Commander", "Captain", "Captain", "Captain"]
+    d = ["Command", "SCience", "Command", "Command", "Command"]
+    id = ["SC 937-0176 CEC", "S 179-276 SP", "SP 937-215", "SJ 854-336", "SS 682-190"]
 
+    return n, r, d, id
+
+def display_menu():
+    
