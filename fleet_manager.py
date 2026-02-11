d2d55620db0ba0666c392922c192ee17c1341d01
Alaniss273 <ah2195@kent.ac.uk>
2026-02-11 14:49:42 +0000

    qj

--- fleet_manager.py
+++ fleet_manager.py
@@ -1,7 +1,7 @@
 def init_database():
     n = ["Kirk", "Spock", "Picard", "Janeway", "Sisko"]
     r = ["Captain", "Commander", "Captain", "Captain", "Captain"]
-    d = ["Command", "SCience", "Command", "Command", "Command"]
+    d = ["Command", "Science", "Command", "Command", "Command"]
     ids = ["SC 937-0176 CEC", "S 179-276 SP", "SP 937-215", "SJ 854-336", "SS 682-190"]
 
     return n, r, d, ids
@@ -9,8 +9,8 @@ def init_database():
 n, r, d, ids = init_database()
 
 def display_menu():   
-    print("Welcome Fleet Manager!")
     un = input("What is your full name?")
+    print("\nWelcome Fleet Manager! - " + un)
 
     print("\nOptions Menu:")
     print("1. Add member")
@@ -22,9 +22,10 @@ def display_menu():
     print("7. Calculate payroll")
     print("8. Count officers")
     
-opt = input("Select option: ")
+    opt = input("Select option: ")
+    return opt
 
-def add_member():
+def add_member(n, r, d, ids):
         
     nid = input("What is the new ID?")
     if nid in ids:
@@ -43,7 +44,7 @@ def add_member():
 
     print("Member added successfully.")
         
-def remove_member():
+def remove_member(n, r, d, ids):
         wid = input("What is the ID?")
         if wid not in ids:
             print("ID cannot be found.")
@@ -57,7 +58,7 @@ def remove_member():
 
         print("Member has been removed.")
 
-def update_rank():
+def update_rank(n, r, ids):
         wid = input("What is the ID?")
         if wid not in ids:
             print("ID cannot be found.")
@@ -67,20 +68,29 @@ def update_rank():
         print("Rank is successfully updated.")
         return
 
-def display_roster():
+def display_roster(n, r, d, ids):
      for i in range(len(n)):
-          print(n[i] + " - " + r[i] + " - " + d[i] + " - " + ids[i])
+        print(n[i] + " - " + r[i] + " - " + d[i] + " - " + ids[i])
 
-def search_crew():
+def search_crew(n, r, d, ids):
      sc = input("What do you want to look up?")
      for i in range(len(n)):
-          if sc in n[i]:
-               print(n[i], r[i], d[i], ids[i])
+        if sc in n[i]:
+            print(n[i], r[i], d[i], ids[i])
 
-def filter_by_division():
-     
+def filter_by_division(n, d):
+    fbd = input("What division are you looking for?")
 
-def calculate_payroll():
+    found = False
+    for i in range(len(d)):
+        if d[i] == fbd:
+            print(n[i])
+            found = True
+
+    if not found:
+        print("No one was found in that division.")
+
+def calculate_payroll(r):
     pay = 0
     for r in r:
         if r == "Captain":
@@ -89,7 +99,7 @@ def calculate_payroll():
              pay += 300
     return pay 
 
-def count_officers():
+def count_officers(r):
     count = 0
     for r in r:
         if r == "Captain" or r == "Commander":
@@ -102,24 +112,31 @@ def main():
     while True:
         opt = display_menu()
 
-        if choice == "1":
+        if opt == "1":
             add_member(n, r, d, ids)
         
-        elif choice == "2":
+        elif opt == "2":
             remove_member(n, r, d, ids)
 
-        elif choice == "3":
+        elif opt == "3":
             update_rank(n, r, ids)
 
-        elif choice == "4":
+        elif opt == "4":
+            display_roster(n, r, d, ids)
+
+        elif opt == "5":
             search_crew(n, r, d, ids)
 
-        elif choice == "5":
+        elif opt == "6":
             filter_by_division(n, d)
 
-        elif choice == "6":
+        elif opt == "7":
             calculate_payroll(r)
 
-        elif choice == "7":
+        elif opt == "8":
             count_officers(r)
+
+        else: 
+            print("Invalid option.")
+
 main()
